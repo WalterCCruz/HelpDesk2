70f30c74c051ac0ab89e77964f18182c12814fab
WaltecCruz <myusername@myemaildomain.com>
2018-05-29 16:30:04 -0300

    Atualizacao src

---
+++ src/main/java/com/WalterCruz/helpdesk/api/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.WalterCruz.helpdesk.api.security;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"Unauthorized");
+	}
+		
+	
+}
---
+++ src/main/java/com/WalterCruz/helpdesk/api/security/WebSecurityConfig.java
@@ -0,0 +1,5 @@
+package com.WalterCruz.helpdesk.api.security;
+
+public class WebSecurityConfig {
+
+}
